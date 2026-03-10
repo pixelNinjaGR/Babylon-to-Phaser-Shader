ff664531b43329205ae23e055ea944e36770ab80
pixelNinjaGR <106584584+pixelNinjaGR@users.noreply.github.com>
2026-03-10 17:50:09 +0530

    chore: clean up test files and boilerplate

--- testRegress.js
+++
@@ -1,9 +0,0 @@
-import { parseNMEtoPhaser } from './src/shaderConverter.js';
-import fs from 'fs';
-
-const code = fs.readFileSync('./Panner_1.txt', 'utf8');
-const r = parseNMEtoPhaser(code);
-console.log('GLSL lines:', r.glsl.split('\n').length);
-console.log('uniforms:', r.uniforms);
-console.log('hasAlpha:', r.hasAlpha);
-console.log('warnings:', r.warnings);
